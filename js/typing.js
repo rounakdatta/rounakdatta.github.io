d7529aad21aeafb85388ca4b16468e9b574ce212
rounakdatta <rounakdatta12@gmail.com>
2018-08-03 00:31:57 +0530

    achievements added

--- js/typing.js
+++ js/typing.js
@@ -5,7 +5,7 @@ $(function(){
   var subtitles = [
   " I'm 20! It's just the beginning of youth! ^1000 Best time for Entrepreneurship! ^1000 the Best time to be Alive!  ^500 ! ^500 !",
   " You found me! Was it ^1000 GitHub? ^1000 or Quora? ^2000 or Facebook? ^500 ? ^500 ?",
-  " Either I'm typing ^1000 or on Music ^1000 or discussing Startups ^1000 or reading HN ^2000 or Sleeping ^500 💤 ^500 💤 ^500 💤"
+  " How bad are my design skills? ^1000 Not-So-Good? ^1000 Terrible? ^1000 Terribly Terrible ^500 ! ^500 ! ^500 ?"
   ];
   $(".element").typed({
     strings: ["", subtitles[Math.floor((Math.random() * subtitles.length))]],
